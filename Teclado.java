7fd0c803c11b5e7e5e5f0ce6fbf3b4e82fa6ed05
manolovillar <francisco.manuel.villar.fernandez@gmail.com>
2019-01-14 10:51:21 +0100

    miprimer

---
+++ Teclado.java
@@ -0,0 +1,56 @@
+package listado3ArraysJavierLoperaJimenez;
+
+import java.util.Scanner;
+
+public class Teclado {
+
+	
+	public static void main(String[] args) {
+		
+		
+	}
+	public static boolean nextBoolean(){
+		
+		Scanner sc = new Scanner(System.in);
+		return sc.nextBoolean();
+		
+	}
+	public static int nextInt(){
+		
+		Scanner sc = new Scanner(System.in);
+		return sc.nextInt();
+		
+	}
+	public static short nextShort(){
+		
+		Scanner sc = new Scanner(System.in);
+		return sc.nextShort();
+		
+	}
+	public static long nextLong(){
+		
+		Scanner sc = new Scanner(System.in);
+		return sc.nextLong();
+		
+	}
+	public static float nextFloat(){
+		
+		Scanner sc = new Scanner(System.in);
+		return sc.nextFloat();
+		
+	}
+	public static double nextDouble(){
+		
+		Scanner sc = new Scanner(System.in);
+		return sc.nextDouble();
+		
+	}
+	public static String nextLine(){
+		
+		Scanner sc = new Scanner(System.in);
+		return sc.nextLine();
+		
+	}
+	
+
+}
